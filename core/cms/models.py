9ee5514216895cd6db165c140a020d979aef0b64
esmaeil chegeni <best.rank@ymail.com>
2020-08-07 13:53:53 +0430

    edit models

--- core/cms/models.py
+++ core/cms/models.py
@@ -7,9 +7,9 @@ class post (models.Model):
     title = models.CharField(max_length=100, verbose_name="عنوان")
     description = models.TextField(max_length=1000, verbose_name="محتوا")
     date = models.DateTimeField(auto_now_add= True, verbose_name="زمان انتشار")
-    thumbnail = models.ImageField(verbose_name="تصویر شاخص")
+    thumbnail = models.ImageField(upload_to='images/', verbose_name="تصویر شاخص")
     tags = TaggableManager(verbose_name="برچسب ها")
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,verbose_name="نویسنده")
 
     class meta:
         verbose_name = "پست"
