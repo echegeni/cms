66a5632944caaa1036818c9dfa495787adf6cd3c
esmaeil chegeni <best.rank@ymail.com>
2020-08-07 13:50:17 +0430

    add models

--- core/cms/models.py
+++ core/cms/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 # Create your models here.
 class post (models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000)
-    
+    title = models.CharField(max_length=100, verbose_name="عنوان")
+    description = models.TextField(max_length=1000, verbose_name="محتوا")
+    date = models.DateTimeField(auto_now_add= True, verbose_name="زمان انتشار")
+    thumbnail = models.ImageField(verbose_name="تصویر شاخص")
+    tags = TaggableManager(verbose_name="برچسب ها")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class meta:
+        verbose_name = "پست"
+        verbose_name_plural = "پست ها"
+
+    def __str__(self):
+      return self.title
